0e19a352729a9850b76d15af1c9983fbe7364b52
DESKTOP-TJDDME2\Pincha <pinchukovartur@gmail.com>
2018-10-30 22:29:05 +0300

    added admin panel model fields

--- prestige/admin.py
+++ prestige/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Prestige
 
-admin.site.register(Prestige)
+
+class PrestigeAdmin(admin.ModelAdmin):
+    list_display = ('game_state_id', 'name', 'prestige', 'created_at', 'updated_at')
+
+admin.site.register(Prestige, PrestigeAdmin)
