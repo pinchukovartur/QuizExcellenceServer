259369fa62b03e633b52cc97aaf656d25bc6d4b4
pinchukovartur <pinchukovartur@gmail.com>
2019-06-01 23:25:42 +0300

    change setting

--- core/settings.py
+++ core/settings.py
@@ -116,5 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = "/usr/local/lib/python3.4/dist-packages/django/contrib/admin/static/"
