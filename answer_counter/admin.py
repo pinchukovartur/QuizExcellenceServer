b648630dd699563be8a18dd49f98b566bd34c7d1
pinchukovartur <pinchukovartur@gmail.com>
2020-12-14 07:12:42 +0300

    added stats fields in admin

--- answer_counter/admin.py
+++ answer_counter/admin.py
@@ -4,7 +4,13 @@ from .models import AnswerCounter
 
 
 class AnswerCounterAdmin(admin.ModelAdmin):
-    list_display = ('quest_hash', 'first_answer', 'second_answer', 'third_answer', 'four_answer', 'updated_at')
+    def complexity(self, obj):
+        sum_all = (obj.first_answer + obj.second_answer + obj.third_answer + obj.four_answer)
+        if sum_all == 0:
+            return 0
+        return round((sum_all - obj.first_answer) / sum_all, 2)
+
+    list_display = ('quest_hash', 'complexity', 'first_answer', 'second_answer', 'third_answer', 'four_answer', 'updated_at')
 
 
 admin.site.register(AnswerCounter, AnswerCounterAdmin)
--- likes/admin.py
+++ likes/admin.py
@@ -4,7 +4,12 @@ from likes.models import Likes
 
 
 class LikesAdmin(admin.ModelAdmin):
-    list_display = ('quest_hash', 'likes', 'dislikes', 'updated_at')
+    def percent_dislikes(self, obj):
+        if obj.likes == 0 or obj.dislikes == 0:
+            return 0
+        return round(obj.dislikes / (obj.dislikes + obj.likes), 2)
+
+    list_display = ('quest_hash', 'likes', 'dislikes', 'updated_at', 'percent_dislikes')
 
 
 admin.site.register(Likes, LikesAdmin)
